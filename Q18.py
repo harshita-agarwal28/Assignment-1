0aeeab26349bf3ec1e7077da39723748491c8509
harshita-agarwal28 <144692289+harshita-agarwal28@users.noreply.github.com>
2024-06-15 17:44:19 +0530

    Delete Q18.py

--- Q18.py
+++
@@ -1,9 +0,0 @@
-s1=input("Enter string 1:")
-s2=input("Enter string 2:")
-rev=''
-for i in range(len(s2)-1,-1,-1):
-    rev+=s2[i]
-if s1==rev:
-    print("Anagram")
-else:
-    print("Not Anagram")
